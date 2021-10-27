4d19283afeee14ce307aeaa133fc2f8ec9f88cf3
Hajiyev <tu201806256@aztu.local>
2021-10-27 09:38:51 +0400

    task 2, task3 solved

--- 15.27.10.2021-class-task/Program.cs
+++ 15.27.10.2021-class-task/Program.cs
@@ -7,36 +7,63 @@ namespace _15._27._10._2021_class_task
         static void Main(string[] args)
         {
             #region Task 1
-            Console.WriteLine("1, 2, 3 => " + SumOf3(1, 2, 3));
-            Console.WriteLine("1, 3, 2 => " + SumOf3(1, 3, 2));
-            Console.WriteLine("3, 1, 2 => " + SumOf3(3, 1, 2));
+            //Console.WriteLine("max(1, 2, 3) = " + MaxOf3(1, 2, 3));
+            //Console.WriteLine("max(1, 3, 2) = " + MaxOf3(1, 3, 2));
+            //Console.WriteLine("max(3, 1, 2) = " + MaxOf3(3, 1, 2));
+            #endregion
+
+            #region Task 2
+            //MySwap(5, 7);
+            #endregion
+
+            #region Task 3
+            Console.WriteLine("Sum of digits of 12345: " + SumOfDigits(12345));
             #endregion
         }
 
-        static int SumOf3(int a, int b, int c)
+        static int MaxOf3(int a, int b, int c)
         {
-            if (a > b)
+            return a > b ? (a > c ? a : c) : (b > c ? b : c);
+            #region alternative 1
+            //if (a > b)
+            //{
+            //    if (a > c)
+            //    {
+            //        return a;
+            //    }
+            //    else
+            //    {
+            //        return c;
+            //    }
+            //}
+            //else
+            //{
+            //    if (b > c)
+            //    {
+            //        return b;
+            //    }
+            //    else
+            //    {
+            //        return c;
+            //    }
+            //}
+            #endregion
+        }
+
+        static void MySwap(int a, int b)
+        {
+            Console.WriteLine($"Now the 1st number is {(a > b ? a : b)}, and the 2nd number is {(a < b ? a : b)}");
+        }
+
+        static int SumOfDigits(int num)
+        {
+            int sum = 0;
+            while (num != 0)
             {
-                if (a > c)
-                {
-                    return a;
-                }
-                else
-                {
-                    return c;
-                }
+                sum += num % 10;
+                num /= 10;
             }
-            else
-            {
-                if (b > c)
-                {
-                    return b;
-                }
-                else
-                {
-                    return c;
-                }
-            }
+            return sum;
         }
     }
 }

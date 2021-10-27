89913a09ed6f615dd5fc19363465bf3bda9fef11
Hajiyev <tu201806256@aztu.local>
2021-10-27 08:51:47 +0400

    init, task 1 solved

---
+++ 15.27.10.2021-class-task/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _15._27._10._2021_class_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            Console.WriteLine("1, 2, 3 => " + SumOf3(1, 2, 3));
+            Console.WriteLine("1, 3, 2 => " + SumOf3(1, 3, 2));
+            Console.WriteLine("3, 1, 2 => " + SumOf3(3, 1, 2));
+            #endregion
+        }
+
+        static int SumOf3(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    return a;
+                }
+                else
+                {
+                    return c;
+                }
+            }
+            else
+            {
+                if (b > c)
+                {
+                    return b;
+                }
+                else
+                {
+                    return c;
+                }
+            }
+        }
+    }
+}
